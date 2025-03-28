aa65b37ba1fc44133813857f31b90100bb027730
AkhilRangpariya <akhilrangpariya9494@gmail.com>
2025-03-28 23:30:38 +0530

    basic /test route for  the SSR

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ connectToMongoDB("mongodb://localhost:2701/short-url");
 app.use(express.json());
 app.use("/url", router);
 
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+
+    return res.send(`
+        <html>
+            <head>
+                <title>Go to URL shortener</title>
+            </head>
+            <body>
+                <ol>
+                    ${allUrls.map((url, index) => (`<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length}</li>`).join(''))}
+                </ol >
+            </body >
+        </html > `)
+})
+
 app.get("/:shortId", async (req, res) => {
     const shortId = req.params.shortId;
 
@@ -28,5 +44,5 @@ app.get("/:shortId", async (req, res) => {
     res.redirect(entry.redirectURL);
 })
 
-app.listen(PORT, () => console.log(`Server Started listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Server Started listening on ${PORT} `))
 
